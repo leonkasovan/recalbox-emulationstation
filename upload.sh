3812799ffeb43776366f1da9e3c27ade065022f8
Novan using Ubuntu WSL PC <dhani.novan@gmail.com>
2024-03-30 06:52:02 +0700

    update help and guide

--- upload.sh
+++ upload.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-/home/ubuntu/sysroot/usr/bin/aarch64-buildroot-linux-gnu-strip -s emulationstation
-7zr a emulationstation.7z emulationstation
-sftp root@192.168.1.7 <<END
-put emulationstation.7z
-END
-rm emulationstation.7z
-rm emulationstation
+cp emulationstation ../recalbox-rg353x/output/target/usr/bin/
+cp recalbox_help.txt ../recalbox-rg353x/output/target/recalbox/share_init/system/.emulationstation/
