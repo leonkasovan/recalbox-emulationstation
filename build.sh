aeee5090173b85ba14a947f1c4ccdbeb7e5535a5
Novan using Ubuntu WSL PC <dhani.novan@gmail.com>
2024-04-11 03:32:03 +0700

    add debug build

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
+rm CMakeCache.txt
 cmake . \
  -DOPTION_RECALBOX_PRODUCTION_BUILD=true \
  -DCMAKE_C_FLAGS="-mabi=lp64 -mcpu=cortex-a55" \
  -DCMAKE_CXX_FLAGS="-mabi=lp64 -mcpu=cortex-a55" \
  -DBUILD_SHARED_LIBS=ON
  make -j8
+
+# Build with Debug
+#  cmake . \
+#  -DOPTION_RECALBOX_PRODUCTION_BUILD=true \
+#  -DCMAKE_BUILD_TYPE=Debug \
+#  -DCMAKE_C_FLAGS="-mabi=lp64 -mcpu=cortex-a55" \
+#  -DCMAKE_CXX_FLAGS="-mabi=lp64 -mcpu=cortex-a55" \
+#  -DBUILD_SHARED_LIBS=ON
+#  make -j8
